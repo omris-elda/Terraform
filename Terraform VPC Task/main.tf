296aa9613dbbe6ff4ed86de8bc878124bf56dc56
Omris-Elda <ed.prickett@hotmail.com>
2020-09-11 13:59:30 +0100

    First try at VPC exercise

---
+++ Terraform VPC Task/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "aws-ireland"
+}
+# Create the VPC
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+# Create the Subnet
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    depends_on = 
+}
+# Create the Security Group
+resource "aws_security_group" "Default_SecGrp" {
+    name = "allow_ssh"
+    description = "Allows inbound SSH traffic and all outbound traffic"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "Allow incoming SSH traffic"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+# Ingress and egress rules can be specified multiple times in order to have multiple rules
+    egress {
+        description = "Allow all outbound traffic"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+# Terraform auto-removes the default allow all rule for egress traffic, and so we have to manually
+# recreate it with the above code
+}
+# Create the internet gateway
+resource "aws_internet_gateway" "gateway" {
+    vpc_id = aws_vpc.main.id
+}
+# Create an EC2 Instance
+resource "aws_instance" "EC2" {
+    ami = var.ami-id
+    instance_type = var.instance_type
+    key_name = var.ssh-key
+
+    depends_on = [aws_internet_gateway.gateway]
+    vpc_security_group_ids = aws_security_group.Default_SecGrp.id
+    subnet_id = aws_subnet.main.id
+}
+# Create a Route Table
+resource "aws_route_table" "routetable" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "10.0.1.0/24"
+        gateway_id = aws_internet_gateway.gateway.id
+    }
+}
---
+++ Terraform VPC Task/variables.tf
@@ -0,0 +1,14 @@
+variable "ami-id" {
+  description = "AMI ID of ubuntu 18.04LTS eu-west-1"
+  default     = "ami-04137ed1a354f54c4"
+}
+
+variable "instance-type" {
+  description = "Free tier EC2 Instance type"
+  default     = "t2.micro"
+}
+
+variable "ssh-key" {
+  description = "Associated Key to SSH into the EC2 Instance"
+  default     = "Ed-Home-PC"
+}
