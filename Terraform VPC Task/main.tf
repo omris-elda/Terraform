a69345bd98191f64aeb431a346266cb3e269a98b
Ubuntu <ubuntu@ip-172-31-41-71.eu-west-1.compute.internal>
2020-09-11 13:37:22 +0000

    Added formatting

--- Terraform VPC Task/main.tf
+++ Terraform VPC Task/main.tf
@@ -47,9 +47,9 @@ resource "aws_instance" "EC2" {
   instance_type = var.instance-type
   key_name      = var.ssh-key
 
-  depends_on             = [aws_internet_gateway.gateway]
-  vpc_security_group_ids = [aws_security_group.Default_SecGrp.id]
-  subnet_id              = aws_subnet.main.id
+  depends_on                  = [aws_internet_gateway.gateway]
+  vpc_security_group_ids      = [aws_security_group.Default_SecGrp.id]
+  subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
 }
 # Create a Route Table
