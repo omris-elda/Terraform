21916e448d8d03a2e2c26cfd3f4308e6c15d0fce
Omris-Elda <ed.prickett@hotmail.com>
2020-09-14 14:59:33 +0100

    Changed value to default in variables file

--- Intermediate Syntax/variables.tf
+++ Intermediate Syntax/variables.tf
@@ -8,5 +8,5 @@ variable "centos-ami-id" {
   description = "The CentOS ami ID for AWS"
   # For the purposes of this exercise it's not actually CentOS, 
   # but an older version of Ubuntu.
-  value = "ami-09b49c48928db765c"
+  default = "ami-09b49c48928db765c"
 }
