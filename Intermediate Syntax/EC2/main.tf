58cd649f1df1d7ae1927cd1ce512637c3c644cfd
Omris-Elda <ed.prickett@hotmail.com>
2020-09-14 15:29:14 +0100

    Changed ami variable name

--- Intermediate Syntax/EC2/main.tf
+++ Intermediate Syntax/EC2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Instances" {
-  ami                    = var.ubuntu-ami
+  ami                    = var.ami
   instance_type          = var.instance-type
   key_name               = var.pem-key
   subnet_id              = var.subnet_id
