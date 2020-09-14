c3bf51f9b533b7f4ba4bd2ba8a327ea8874f6e66
Omris-Elda <ed.prickett@hotmail.com>
2020-09-14 12:49:00 +0100

    made variable a list

--- Intermediate Syntax/SecurityGroup/main.tf
+++ Intermediate Syntax/SecurityGroup/main.tf
@@ -10,13 +10,13 @@ resource "aws_security_group" "wsg" {
       from_port   = port.value
       to_port     = port.value
       protocol    = "tcp"
-      cidr_blocks = var.open-internet
+      cidr_blocks = [var.open-internet]
     }
   }
   egress {
     from_port   = var.outbound-port
     protocol    = "-1"
     to_port     = var.outbound-port
-    cidr_blocks = var.open-internet
+    cidr_blocks = [var.open-internet]
   }
 }
