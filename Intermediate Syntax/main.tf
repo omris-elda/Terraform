dbcea2a156a773c2257fa162690ab7e73c7b03e6
Ubuntu <ubuntu@ip-172-31-41-71.eu-west-1.compute.internal>
2020-09-14 11:33:25 +0000

    Added formatting

--- Intermediate Syntax/main.tf
+++ Intermediate Syntax/main.tf
@@ -5,21 +5,21 @@ provider "aws" {
 }
 
 module "aws_vpc" {
-    source = "./VPC"
+  source = "./VPC"
 }
 
 module "aws_webserver_sg" {
-    source = "./SecurityGroups"
-    name = "WebServerSG"
-    vpc_id = module.aws_vpc.vpc_id
+  source = "./SecurityGroups"
+  name   = "WebServerSG"
+  vpc_id = module.aws_vpc.vpc_id
 }
 
 module "webserver_node" {
-    source = "./EC2"
-    subnet_id = module.aws_vpc.public_subnetA_id
-    vpc_security_group_ids = module.aws_webserver_sg.aws_wsg_id
-    tags = {
-        Name = "WebServer_Node"
-    }
-    associate_public_ip_address = true
+  source                 = "./EC2"
+  subnet_id              = module.aws_vpc.public_subnetA_id
+  vpc_security_group_ids = module.aws_webserver_sg.aws_wsg_id
+  tags = {
+    Name = "WebServer_Node"
+  }
+  associate_public_ip_address = true
 }
