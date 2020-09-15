f9743b9b091c4cec6cf167abca6c3bc436f67b56
Omris-Elda <ed.prickett@hotmail.com>
2020-09-15 11:47:27 +0100

    Trying dynamic ansible inventorys

--- SFIA-2-Setup/outputs.tf
+++ SFIA-2-Setup/outputs.tf
@@ -1,4 +1,17 @@
 output "EC2_Node_1_IP" {
   value = "sudo ssh -i ~/.ssh/${module.Node_1.KeyName}.pem ubuntu@${module.Node_1.PublicEC2IP}"
 }
-# In testing
+# Outputs the command to SSH into the Node_1 resource that was just created
+
+
+# The following should create a new Ansible inventory file
+
+resource "local_file" "AnsibleInventory" {
+  content = templatefile("inventory.cfg",
+  {
+    public-ip = aws_instance.Node_1.*.public_ip
+    public-id = aws_instance.Node_1.*.id
+  }
+  )
+  filename = "inventory.cfg"
+}
